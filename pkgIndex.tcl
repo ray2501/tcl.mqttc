2261a9b90cfaa954dced9710c0472ad0e60ebd8c
Danilo Chang <ray2501@gmail.com>
2019-09-10 00:49:49 +0800

    Bump version to v0.6

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded mqttc 0.5 \
-    [list load [file join $dir libmqttc0.5.so] mqttc]
+package ifneeded mqttc 0.6 \
+    [list load [file join $dir libmqttc0.6.so] mqttc]
