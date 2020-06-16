3db7d812909d89a0e3088145d929a63643f603c9
Danilo Chang <ray2501@gmail.com>
2020-06-16 22:15:04 +0800

    Bump version to v0.8

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded mqttc 0.7 \
-    [list load [file join $dir libmqttc0.7.so] mqttc]
+package ifneeded mqttc 0.8 \
+    [list load [file join $dir libmqttc0.8.so] mqttc]
--- tcl.mqttc_SUSE.spec
+++ tcl.mqttc_SUSE.spec
@@ -4,7 +4,7 @@
 
 Name:          tcl.mqttc
 Summary:       MQTT Client for Tcl
-Version:       0.7
+Version:       0.8
 Release:       0
 License:       BSD 3-Clause License
 Group:         Development/Libraries/Tcl
