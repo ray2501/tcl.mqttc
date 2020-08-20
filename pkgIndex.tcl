377015562bc8d33ede4fef1b0e1fd8c89c23d6b3
Danilo Chang <ray2501@gmail.com>
2020-08-20 19:31:35 +0800

    Bump version to v0.9

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded mqttc 0.8 \
-    [list load [file join $dir libmqttc0.8.so] mqttc]
+package ifneeded mqttc 0.9 \
+    [list load [file join $dir libmqttc0.9.so] mqttc]
