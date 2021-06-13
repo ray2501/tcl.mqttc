f531a98c73004c23f884fe3ee33256f9a750c1ef
Danilo Chang <ray2501@gmail.com>
2021-06-13 17:41:57 +0800

    Bump version to v0.13

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,5 @@
 #
 # Tcl package index file
 #
-package ifneeded mqttc 0.12 \
-    [list load [file join $dir libmqttc0.12.so] mqttc]
+package ifneeded mqttc 0.13 \
+    [list load [file join $dir libmqttc0.13.so] mqttc]
--- tcl.mqttc_SUSE.spec
+++ tcl.mqttc_SUSE.spec
@@ -4,7 +4,7 @@
 
 Name:          tcl.mqttc
 Summary:       MQTT Client for Tcl
-Version:       0.12
+Version:       0.13
 Release:       0
 License:       BSD-3-Clause
 Group:         Development/Libraries/Tcl
