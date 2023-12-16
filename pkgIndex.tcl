ddba97c53cefcde215e6d37fa5600fba41a1d5e3
Danilo Chang <ray2501@gmail.com>
2023-12-16 19:40:21 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded mqttc 0.15 \
-    [list load [file join $dir libmqttc0.15.so] mqttc]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded mqttc 0.15 \
+	    [list load [file join $dir libtcl9mqttc0.15.so] [string totitle mqttc]]
+} else {
+    package ifneeded mqttc 0.15 \
+	    [list load [file join $dir libmqttc0.15.so] [string totitle mqttc]]
+}
