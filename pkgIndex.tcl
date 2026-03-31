262a3d0f476591fe94507a68c502d8743fa03bf6
Danilo Chang <ray2501@gmail.com>
2026-03-31 11:16:57 +0800

    Bump version to v0.20

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded mqttc 0.19 \
-	    [list load [file join $dir libtcl9mqttc0.19.so] [string totitle mqttc]]
+    package ifneeded mqttc 0.20 \
+	    [list load [file join $dir libtcl9mqttc0.20.so] [string totitle mqttc]]
 } else {
-    package ifneeded mqttc 0.19 \
-	    [list load [file join $dir libmqttc0.19.so] [string totitle mqttc]]
+    package ifneeded mqttc 0.20 \
+	    [list load [file join $dir libmqttc0.20.so] [string totitle mqttc]]
 }
